ec430a0de80bddd2b7eed884cbc0ebfe16c9170d
Emiel <chasinmiel@hotmail.com>
2023-02-10 16:11:01 -0300

    Update signer.rb

--- lib/signer.rb
+++ lib/signer.rb
@@ -150,7 +150,7 @@ class Signer
         set_namespace_for_node(@signature_node, DS_NAMESPACE, ds_namespace_prefix)
         
         # change Emiel: 
-        security_node.children.before(@signature_node)
+        security_node.children.first.add_previous_sibling(@signature_node)
         # ORG: security_node.add_child(@signature_node)
       end
       @signature_node
